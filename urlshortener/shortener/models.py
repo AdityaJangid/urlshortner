6bbd20eae3b11795802d52b674012b535ed68d8d
AdityaJangid <eradityajangid@gmail.com>
2017-01-04 23:16:00 +0530

    created models.py

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class BigUrl(models.Model):
+    url = models.CharField(max_length=220,)
+    shortcode = models.CharField(max_length=15,default="xyz",unique=True)
+
+    def __str__(self):
+        return str(self.url)

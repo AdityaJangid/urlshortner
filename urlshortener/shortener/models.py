9ad5d7eb9c620f6d72da5e685876feaeb043f6e3
AdityaJangid <eradityajangid@gmail.com>
2017-01-06 21:48:33 +0530

    added timestamp

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class BigUrl(models.Model):
     url = models.CharField(max_length=220,)
     shortcode = models.CharField(max_length=15,default="xyz",unique=True)
+    last_modified= models.DateTimeField(auto_now=True)
+    time_stamp= models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.url)

342bb3c19b82b0f039b9c8853bb3cbd383a3f30b
temporaryaccount3 <adityajangid2018@gmail.com>
2017-01-16 23:25:10 +0530

    overrided save method

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -7,6 +7,11 @@ class BigUrl(models.Model):
     shortcode = models.CharField(max_length=15,default="xyz",unique=True)
     last_modified= models.DateTimeField(auto_now=True)
     time_stamp= models.DateTimeField(auto_now_add=True)
+    
+    def save(self,*argv,**kwargs):
+        print("something")
+        self.shortcode = code_generator()
+        super(BigUrl,self).save(*argv,**kwargs)
 
     def __str__(self):
         return str(self.url)

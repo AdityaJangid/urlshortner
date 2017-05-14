0aef5f9ea3c9205aa9da44bd993a707a1e057ecf
AdityaJangid <eradityajangid@gmail.com>
2017-05-14 16:29:12 +0530

    code_generator

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+import random
 
 # Create your models here.
+def code_generator():
+    a = "abcdefghijklmnopqrstuvwxyz"
+    return "".join(random.sample(a*6,6))
+
 
 class BigUrl(models.Model):
     url = models.CharField(max_length=220,)
     shortcode = models.CharField(max_length=15,default="xyz",unique=True)
     last_modified= models.DateTimeField(auto_now=True)
     time_stamp= models.DateTimeField(auto_now_add=True)
-    
+
     def save(self,*argv,**kwargs):
         print("something")
         self.shortcode = code_generator()

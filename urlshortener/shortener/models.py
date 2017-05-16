195c2b2b903333082c024d776248e0ed9f3415fb
AdityaJangid <eradityajangid@gmail.com>
2017-05-16 10:12:43 +0530

    refresh all code at once

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -1,12 +1,38 @@
 from django.db import models
 from .utils import Code_generator
+from .utils import create_shortcode
 
 # Create your models here.
 class BigUrlManager(models.Manager):
+    # gives all the unactive urls
+
     def unactive(self, *args, **kwargs):
         query_set = super(BigUrlManager, self).all(*args, **kwargs)
         query_set = query_set.filter(Active = False)
         return query_set
+
+
+    #gives all the  active urls
+
+    def active(self, *args, **kwargs):
+        query_set = super(BigUrlManager, self).all(*args, **kwargs)
+        query_set = query_set.filter(Active = True)
+        return query_set
+    
+
+    #change all the shortcodes all at  once
+
+    def refresh_shortcodes(self):
+        new_codes = 0
+        qs = BigUrl.objects.filter(id__gte=1) 
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q)
+            print(q.shortcode)
+            #  print(q.objects.filter(id=q))
+            q.save()
+            new_codes = new_codes+1
+        return "New codes made: {i}".format(i=new_codes)
 
 class BigUrl(models.Model):
     url = models.CharField(max_length=220,)
@@ -19,7 +45,7 @@ class BigUrl(models.Model):
 
     def save(self,*argv,**kwargs):
         if self.shortcode == "" or self.shortcode is None:
-          self.shortcode = short_code(self)
+          self.shortcode = create_shortcode(self)
         super(BigUrl,self).save(*argv,**kwargs)
 
     def __str__(self):

21f2c9eee78ae90044f54c29709bce2d32050686
AdityaJangid <eradityajangid@gmail.com>
2017-05-17 13:36:38 +0530

    replace value by global min variable

--- urlshortener/shortener/utils.py
+++ urlshortener/shortener/utils.py
@@ -1,11 +1,14 @@
+from django.conf import settings
 import random
 import string
 
-def Code_generator(size =6 ):
-    a = string.ascii_letters + string.digits
-    return "".join(random.sample(a*6,6))
+SHORTCODE_MIN = getattr( settings,"SHORTCODE_MIN" ,6)
 
-def create_shortcode(instance,size=6):
+def Code_generator(size =SHORTCODE_MIN ):
+    a = string.ascii_letters + string.digits
+    return "".join(random.sample(a*SHORTCODE_MIN,SHORTCODE_MIN))
+
+def create_shortcode(instance,size=SHORTCODE_MIN):
     new_code= Code_generator(size= size )
     instance_class = instance.__class__
     query_set = instance_class.objects.filter(shortcode=new_code).exists()

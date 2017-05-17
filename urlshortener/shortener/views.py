150b712fa730a7373efb4d17e724749a573ee957
AdityaJangid <eradityajangid@gmail.com>
2017-05-17 13:34:09 +0530

    created views

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+from django.views import View
+
+
+
+def Redirect_FB_View(request,*args,**kwargs):
+    return  HttpResponse("hello")
+
+
+
+class Redirect_CB_View(View):
+    def get(self, request, *args,**kwargs):
+        return HttpResponse("hello again")

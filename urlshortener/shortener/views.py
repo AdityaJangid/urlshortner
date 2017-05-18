d01f8bae5847f78ea593d0e330a2325fa2bcc2ff
AdityaJangid <eradityajangid@gmail.com>
2017-05-18 22:26:14 +0530

    connected shortcodes

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 # Create your views here.
 from django.views import View
+from .models import BigUrl
 
 
 
-def Redirect_FB_View(request,*args,**kwargs):
-    return  HttpResponse("hello")
+def Redirect_FB_View(request,shortcode = None, *args,**kwargs):
+    #  try:
+    #      obj = BigUrl.objects.get(shortcode = shortcode)
+    #  except:
+    #      obj  = BigUrl.objects.all().first()
+
+
+
+    obj_url = None
+    qs = BigUrl.objects.filter(shortcode__iexact = shortcode.upper())
+    if qs.exists() and qs.count() ==1:
+        obj = qs.first()
+        obj_url = obj.url
+    return  HttpResponse("hello {sc}".format(sc=obj__url))
 
 
 
 class Redirect_CB_View(View):
-    def get(self, request, *args,**kwargs):
-        return HttpResponse("hello again")
+    def get(self, request,shortcode=None,  *args,**kwargs):
+        obj = BigUrl.objects.get(shortcode=shortcode)
+        return  HttpResponse("hello again {sc}".format(sc=obj.url))

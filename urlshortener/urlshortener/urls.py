7716e7a5b7e6725b77900481f77eed59f346b61f
AdityaJangid <eradityajangid@gmail.com>
2017-05-18 22:27:14 +0530

    new regex

--- urlshortener/urlshortener/urls.py
+++ urlshortener/urlshortener/urls.py
@@ -19,6 +19,6 @@ from shortener.views import Redirect_FB_View, Redirect_CB_View
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^short1/$', Redirect_FB_View),
-    url(r'^short2/$', Redirect_CB_View.as_view()),
+    url(r'^a/(?P<shortcode>[\w-]+)/$', Redirect_FB_View),
+    url(r'^b/(?P<shortcode>[\w-]+)/$', Redirect_CB_View.as_view()),
 ]

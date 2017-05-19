a6980abc68f8b3343cbc90d1c37de90b05ebd288
AdityaJangid <eradityajangid@gmail.com>
2017-05-19 21:30:21 +0530

    added redirection

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 # Create your views here.
 from django.views import View
 from .models import BigUrl
 
 
+def test_view(request):
+    return HttpResponse("testing pass if you are viewing this page")
 
 def Redirect_FB_View(request,shortcode=None, *args,**kwargs):
+    obj = get_object_or_404(BigUrl, shortcode=shortcode)
+    #  return  HttpResponse("hello {sc}".format(sc=obj.url))
+    return HttpResponseRedirect(obj.url)
+
+
+
     #  try:
     #      obj = BigUrl.objects.get(shortcode = shortcode)
     #  except:
@@ -15,7 +23,6 @@ def Redirect_FB_View(request,shortcode=None, *args,**kwargs):
     #  return HttpResponse("hello {sc}".format(sc=obj))
 
 
-    obj = get_object_or_404(BigUrl, shortcode=shortcode)
     #  obj_url = obj.url
     #
     #
@@ -24,11 +31,13 @@ def Redirect_FB_View(request,shortcode=None, *args,**kwargs):
     #  if qs.exists() and qs.count() ==1:
     #      obj = qs.first()
     #      obj_url = obj.url
-    return  HttpResponse("hello {sc}".format(sc=obj.url))
 
 
 
 class Redirect_CB_View(View):
     def get(self, request,shortcode=None,  *args,**kwargs):
-        obj = BigUrl.objects.get(shortcode=shortcode)
-        return  HttpResponse("hello again {sc}".format(sc=obj.url))
+        obj = get_object_or_404(BigUrl, shortcode=shortcode)
+        #  return  HttpResponse("hello again {sc}".format(sc=obj.url))
+        return HttpResponseRedirect(obj.url)
+
+

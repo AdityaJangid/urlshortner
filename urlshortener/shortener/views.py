3b8e7da50b73e2bfe9479adb879696616dd610ce
AdityaJangid <eradityajangid@gmail.com>
2017-05-22 18:42:44 +0530

    home view

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -9,31 +9,41 @@ from .models import BigUrl
 def test_view(request):
     return HttpResponse("testing pass if you are viewing this page")
 
-def Redirect_FB_View(request,shortcode=None, *args,**kwargs):
-    obj = get_object_or_404(BigUrl, shortcode=shortcode)
-    return HttpResponseRedirect(obj.url)
-    #  return  HttpResponse("hello {sc}".format(sc=obj.url))
-
-    #  try:
-    #      obj = BigUrl.objects.get(shortcode = shortcode)
-    #  except:
-    #      obj  = BigUrl.objects.all().first()
-    #  return HttpResponse("hello {sc}".format(sc=obj))
 
 
-    #  obj_url = obj.url
-    #
-    #
-    #  obj_url = None
-    #  qs = BigUrl.objects.filter(shortcode__iexact = shortcode.upper())
-    #  if qs.exists() and qs.count() ==1:
-    #      obj = qs.first()
-    #      obj_url = obj.url
-    #
-    #  #  obj = BigUrl.objects.get(shortcode=shortcode)
-    #  return HttpResponse("hello {sc}".format(sc=obj_url))
 
 
+
+class HomeView(View):
+    def get(self, request,  *args,**kwargs):
+        return render(request, "shortener/home.html",{})
+
+
+#  def Redirect_FB_View(request,shortcode=None, *args,**kwargs):
+#      obj = get_object_or_404(BigUrl, shortcode=shortcode)
+#      return HttpResponseRedirect(obj.url)
+#      #  return  HttpResponse("hello {sc}".format(sc=obj.url))
+#
+#      try:
+#          obj = BigUrl.objects.get(shortcode = shortcode)
+#      except:
+#          obj  = BigUrl.objects.all().first()
+#      return HttpResponse("hello {sc}".format(sc=obj))
+#
+#
+#      obj_url = obj.url
+#
+#
+#      obj_url = None
+#      qs = BigUrl.objects.filter(shortcode__iexact = shortcode.upper())
+#      if qs.exists() and qs.count() ==1:
+#          obj = qs.first()
+#          obj_url = obj.url
+#
+#      #  obj = BigUrl.objects.get(shortcode=shortcode)
+#      return HttpResponse("hello {sc}".format(sc=obj_url))
+#
+#
 class Redirect_CB_View(View):
     def get(self, request,shortcode=None,  *args,**kwargs):
         obj = get_object_or_404(BigUrl, shortcode=shortcode)

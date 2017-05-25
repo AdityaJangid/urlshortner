00ae6dbc43e6c8c0b81bcca82e309e8e26101d50
AdityaJangid <eradityajangid@gmail.com>
2017-05-25 23:11:13 +0530

    form validation

--- urlshortener/shortener/forms.py
+++ urlshortener/shortener/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from .validators import validate_url, validate_dot_com
 
 
 class SubmitUrlForm(forms.Form):
-    url = forms.CharField(label="Submit Url")
+    url = forms.CharField(label="Submit Url", validators=[validate_url, validate_dot_com])
 

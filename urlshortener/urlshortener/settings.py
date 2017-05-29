d945df87da06ef5e886a9d7793203d5b4dcccc8e
AdityaJangid <eradityajangid@gmail.com>
2017-05-29 13:13:26 +0530

    added dirs for external templates

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -67,8 +67,8 @@ PARENT_HOST = "canis.com:8000"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        #  'DIRS': [os.path.join(BASE_DIR, "templates")],
-        'DIRS' : [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        #  'DIRS' : [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

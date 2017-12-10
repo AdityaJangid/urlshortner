f66772055cd0697dc85bc5e5cdbc1f4db87a46a2
AdityaJangid <eradityajangid@gmail.com>
2017-12-10 15:06:37 +0530

    allowed host

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'cqw3ox3%7olpmw(cb12gyws6(mb)r#i$3uw85o=bdrsk6+o*tk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] #['www.canis.com','canis.com'] #  enter 127.0.0.1    www.canis.com and   127.0.0.1   canis.com in /etc/hosts to use the domain name sevice
 
+ALLOWED_HOSTS =  ['www.canis.com','canis.com','127.0.0.1'] #enter 127.0.0.1    www.canis.com and   127.0.0.1   canis.com in /etc/hosts to use the domain name sevice
 
 # Application definition
 
